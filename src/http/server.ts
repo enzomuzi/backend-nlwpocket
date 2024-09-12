f055ea08212e7f9ff204d67f4727d5f2ff4871fd
Enzo Muzi <enzshadows@gmail.com>
2024-09-12 17:58:36 -0300

    new function

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,9 +1,27 @@
-import fastify from "fastify";
+import fastify from 'fastify'
+import { createGoal } from '../functions/create-goal'
+import z from 'zod'
 
-const app = fastify();
+const app = fastify()
 
-app.listen({
-    port: 3333,
-}).then(() => {
-    console.log(`Server listening on port ${3333}`);
+app.post('/goals', async request => {
+  const createGoalSchema = z.object({
+    title: z.string(),
+    desiredWeeklyFrequency: z.number().int().min(1).max(7),
+  })
+
+  const body = createGoalSchema.parse(request.body)
+
+  await createGoal({
+    title: body.title,
+    desiredWeeklyFrequency: body.desiredWeeklyFrequency,
+  })
 })
+
+app
+  .listen({
+    port: 3333,
+  })
+  .then(() => {
+    console.log(`Server listening on port ${3333}`)
+  })
